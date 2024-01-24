8f9060977fa9b3d0299c770a4fd01a56a4d242b5
lwdjohari <linggawasistha.djohari@outlook.com>
2024-01-24 17:30:46 +0800

    Add extended types in types/extended_type.h

---
+++ inc/nvm/types/extended_type.h
@@ -0,0 +1,19 @@
+#ifndef NVM_CORE_TYPES_V2_EXTENDED_TYPE_H
+#define NVM_CORE_TYPES_V2_EXTENDED_TYPE_H
+
+#include <cstdint>
+
+namespace nvm {
+namespace types {
+namespace extended {
+
+typedef unsigned long long size64_t;
+typedef signed long long sint64_t;
+typedef char cbyte;
+typedef uint8_t nbyte;
+
+} // namespace extended
+} // namespace types
+} // namespace nvm
+
+#endif // NVM_CORE_TYPES_V2_EXTENDED_TYPE_H
