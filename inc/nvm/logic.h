da447b6271fb061ca0b0bbafb8543cef7357edd5
lwdjohari <linggawasistha.djohari@outlook.com>
2024-01-24 18:53:58 +0800

    Add Range<T> struct for easier to use IsIntersect and GetIntersect template.

--- inc/nvm/logic.h
+++ inc/nvm/logic.h
@@ -26,6 +26,50 @@
 namespace nvm {
 namespace logic {
 
+/**
+ * @brief Represents a range of values.
+ *
+ * The Range struct template is used to define a range of values of type T.
+ * Only arithmetic types are allowed for the Range struct template.
+ *
+ * @tparam T The type of values in the range.
+ */
+template <typename T>
+struct Range {
+  static_assert(std::is_arithmetic<T>::value,
+                "Only arithmetic types are allowed for Range struct template.");
+
+  T start; /**< The start value of the range. */
+  T end;   /**< The end value of the range. */
+
+  /**
+   * @brief Default constructor.
+   * Initializes the start and end values to their default values.
+   */
+  explicit Range() : start(T()), end(T()){};
+
+  /**
+   * @brief Constructor with specified start and end values.
+   * @param start The start value of the range.
+   * @param end The end value of the range.
+   */
+  explicit Range(T start, T end) : start(start), end(end){};
+
+  /**
+   * @brief Check if the range is valid.
+   * A range is considered valid if the start value is less than or equal to the
+   * end value.
+   * @return true if the range is valid, false otherwise.
+   */
+  bool IsValid() const { return start <= end; }
+
+  /**
+   * Calculates the distance.
+   * @return The distance.
+   */
+  T Distance()  { return end - start; }
+};
+
 /// @brief Get the value is between range supplied. This method use
 /// greater-or-equal comparator & lesser-or-equal comparator. operator.
 /// @tparam T Arithmatic type value
@@ -58,6 +102,17 @@ bool IsIntersect(const T &start1, const T &end1, const T &start2,
   return (start1 <= end2) && (end1 >= start2);
 };
 
+/// @brief Determines whether two ranges intersect.
+/// @tparam Arithmatic type value.
+/// @param range1 The first range.
+/// @param range2 The second range.
+/// @return True if the ranges intersect, false otherwise.
+template <typename T>
+bool IsIntersect(const Range<T> &range1, const Range<T> &range2) {
+  if (!range1.IsValid() || !range2.IsValid()) return false;
+  return IsIntersect(range1.start, range1.end, range2.start, range2.end);
+};
+
 /// @brief Get the start & end value that intersect. This method use
 /// greater-or-equal comparator & lesser-or-equal comparator.
 /// @tparam T Arithmatic type value
@@ -67,8 +122,8 @@ bool IsIntersect(const T &start1, const T &end1, const T &start2,
 /// @param end2 range end 2
 /// @return std::pair<default(),default()> if is not intersect one another.
 template <typename T>
-bool GetIntersect(const T &start1, const T &end1, const T &start2,
-                  const T &end2) {
+Range<T> GetIntersect(const T &start1, const T &end1, const T &start2,
+                      const T &end2) {
   static_assert(std::is_arithmetic<T>::value,
                 "Only arithmetic types are allowed for GetIntersect template.");
   if ((start1 <= end2) && (end1 >= start2)) {
@@ -76,11 +131,33 @@ bool GetIntersect(const T &start1, const T &end1, const T &start2,
     // Calculate the intersection
     T intersectStart = std::max(start1, start2);
     T intersectEnd = std::min(end1, end2);
-    return {intersectStart, intersectEnd};
+
+    // cppcheck-suppress returnStdMoveLocal
+    return std::move(Range(intersectStart, intersectEnd));
   } else {
     // Return a pair of default-constructed T values to indicate no overlap
-    return {T(), T()};
+
+    // cppcheck-suppress returnStdMoveLocal
+    return std::move(Range(T(), T()));
   }
+};
+
+/**
+ * Calculates the intersection of two ranges.
+ *
+ * @tparam T Arithmatic type value.
+ * @param range1 The first range.
+ * @param range2 The second range.
+ * @return A pair representing the intersection of the two ranges.
+ */
+template <typename T>
+Range<T> GetIntersect(const Range<T> &range1, const Range<T> &range2) {
+  if (!range1.IsValid() || !range2.IsValid()) {
+    // cppcheck-suppress returnStdMoveLocal
+    return std::move(Range(T(), T()));
+  }
+
+  return GetIntersect(range1.start, range1.end, range2.start, range2.end);
 };
 
 }  // namespace logic
