1284879efeea895174a65ab6dc21f1757c7d2909
lwdjohari <154069935+lwdjohari@users.noreply.github.com>
2024-03-02 22:16:12 +0800

    Add Option<T>

---
+++ inc/nvm/option.h
@@ -0,0 +1,29 @@
+template<typename T>
+class Option {
+private:
+    T* value;
+
+public:
+    Option() : value(nullptr) {}
+    Option(const T& value) : value(new T(value)) {}
+    Option(const Option<T>& other) : value(other.value ? new T(*other.value) : nullptr) {}
+    ~Option() { delete value; }
+
+    Option& operator=(const Option<T>& other) {
+        if (this != &other) {
+            delete value;
+            value = other.value ? new T(*other.value) : nullptr;
+        }
+        return *this;
+    }
+
+    bool isSome() const { return value != nullptr; }
+    bool isNone() const { return value == nullptr; }
+
+    T unwrap() const {
+        if (isNone()) {
+            throw std::runtime_error("Attempted to unwrap an Option containing no value.");
+        }
+        return *value;
+    }
+};
