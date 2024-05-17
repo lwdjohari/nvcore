c72565424304a82d6e297ec5437f75b39a61a2fa
lwdjohari <linggawasistha.djohari@outlook.com>
2024-05-18 00:29:36 +0800

    Add license header

--- inc/nvm/containers/record_def.h
+++ inc/nvm/containers/record_def.h
@@ -1,3 +1,22 @@
+/*
+ *  Copyright (c) 2024 Linggawasistha Djohari
+ * <linggawasistha.djohari@outlook.com> Licensed to Linggawasistha Djohari under
+ * one or more contributor license agreements. See the NOTICE file distributed
+ * with this work for additional information regarding copyright ownership.
+ *
+ *  Linggawasistha Djohari licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except in
+ *  compliance with the License. You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 #ifndef NVM_CORE_CONTAINERS_V2_RECORD_DEF_H
 #define NVM_CORE_CONTAINERS_V2_RECORD_DEF_H
 
